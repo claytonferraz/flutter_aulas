9b50b8b4c2e72dc941755a2d8e0b60f092b2ab25
Clayton Ferraz <clayton.andrade@ifro.edu.br>
2022-09-05 15:35:17 -0400

    1.0.6 Tela inicial

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -15,7 +15,76 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: color.AppColor.homePageBackground,
       body: Container(
-        child: Column(children: []),
+        padding: const EdgeInsets.only(
+          top: 70,
+          left: 30,
+          right: 20,
+        ),
+        child: Column(children: [
+          Row(
+            children: [
+              Text(
+                "Treinando",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: color.AppColor.homePageTitle,
+                    fontWeight: FontWeight.w700),
+              ),
+              Expanded(child: Container()),
+              Icon(
+                Icons.arrow_back_ios,
+                size: 20,
+                color: color.AppColor.homePageIcons,
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              Icon(
+                Icons.calendar_today_outlined,
+                size: 20,
+                color: color.AppColor.homePageIcons,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: 20,
+                color: color.AppColor.homePageIcons,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              Text(
+                "Seus cursos",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: color.AppColor.homePageSubtitle,
+                    fontWeight: FontWeight.w700),
+              ),
+              Expanded(child: Container()),
+              Text(
+                "Detalhes",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: color.AppColor.homePageDetail,
+                    fontWeight: FontWeight.w400),
+              ),
+              Icon(
+                Icons.arrow_forward,
+                size: 20,
+                color: color.AppColor.homePageIcons,
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+            ],
+          ),
+        ]),
       ),
     );
   }
